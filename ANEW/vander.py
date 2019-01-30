8c744f50b9e94d57fc96e731e30b3ef68642cc3f
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2019-01-30 17:43:37 -0500

    ultimos cambios guia

--- ANEW/vander.py
+++ ANEW/vander.py
@@ -3,25 +3,30 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 #from vaderSentiment import SentimentIntensityAnalyzer
 
 # --- examples -------
-sentences = ["VADER is smart, handsome, and funny.",  # positive sentence example
-             "VADER is smart, handsome, and funny!",  # punctuation emphasis handled correctly (sentiment intensity adjusted)
-             "VADER is very smart, handsome, and funny.", # booster words handled correctly (sentiment intensity adjusted)
-             "VADER is VERY SMART, handsome, and FUNNY.",  # emphasis for ALLCAPS handled
-             "VADER is VERY SMART, handsome, and FUNNY!!!", # combination of signals - VADER appropriately adjusts intensity
-             "VADER is VERY SMART, uber handsome, and FRIGGIN FUNNY!!!", # booster words & punctuation make this close to ceiling for score
-             "VADER is not smart, handsome, nor funny.",  # negation sentence example
-             "The book was good.",  # positive sentence
-             "At least it isn't a horrible book.",  # negated negative sentence with contraction
-             "The book was only kind of good.", # qualified positive sentence is handled correctly (intensity adjusted)
-             "The plot was good, but the characters are uncompelling and the dialog is not great.", # mixed negation sentence
-             "Today SUX!",  # negative slang with capitalization emphasis
-             "Today only kinda sux! But I'll get by, lol", # mixed sentiment example with slang and constrastive conjunction "but"
-             "Make sure you :) or :D today!",  # emoticons handled
-             "Catch utf-8 emoji such as such as 💘 and 💋 and 😁",  # emojis handled
-             "That is actually very sad"  # Capitalized negation
+sentences = ["Contented",  # positive sentence example
+             "Serene",  # punctuation emphasis handled correctly (sentiment intensity adjusted)
+             "relaxed", # booster words handled correctly (sentiment intensity adjusted)
+             "calm",  # emphasis for ALLCAPS handled
+             "lethargic", # combination of signals - VADER appropriately adjusts intensity
+             "bored", # booster words & punctuation make this close to ceiling for score
+             "depressed",  # negation sentence example
+             "sad",  # positive sentence
+             "upset",  # negated negative sentence with contraction
+             "stressed", # qualified positive sentence is handled correctly (intensity adjusted)
+             "nervous", # mixed negation sentence
+             "tense",  # negative slang with capitalization emphasis
+             "alert", # mixed sentiment example with slang and constrastive conjunction "but"
+             "excited",  # emoticons handled
+             "elated",  # emojis handled
+             "happy"  # Capitalized negation
              ]
 
 analyzer = SentimentIntensityAnalyzer()
+dic = {}
 for sentence in sentences:
     vs = analyzer.polarity_scores(sentence)
+    dic[sentence]=vs['compound']
     print("{:-<65} {}".format(sentence, str(vs)).encode("utf-8"))
+
+for key, value in sorted(dic.items(), key=lambda item: (item[1], item[0])):
+    print ("%s: %s" % (key, value))
